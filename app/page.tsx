755b16fe8115c6c62cb5b2e3626366d6b08bd69e
jieun <lesson0186@gmail.com>
2025-02-03 22:55:53 +0900

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
-  return (
-    <main>
-     Hi
-    </main>
-  );
+    return (
+        <main className="flex items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-2xl font-bold text-gray-700">This page is under construction 🚧</h1>
+        </main>
+    );
 }
