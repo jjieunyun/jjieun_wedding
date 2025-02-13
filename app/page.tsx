3c8c780dd91f3bed72fa453abc8263dd90d5fb9d
jieun <lesson0186@gmail.com>
2025-02-13 22:40:56 +0900

    feat:tailwind설정 & temp이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import temp from '@image/IMG_4133.jpg';
+
 export default function Home() {
     return (
-        <main className="flex items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-2xl font-bold text-gray-700">This page is under construction 🚧</h1>
+        <main className="w-full h-full min-h-screen  flex items-center justify-center  bg-black ">
+            <section className={'w-full h-full min-h-screen max-w-[740px] flex items-center bg-black'}>
+                <Image src={temp} alt={'temp'} className={'w-full h-screen object-contain'}/>
+            </section>
         </main>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,108 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-export default {
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+const px0_2000 = { ...Array.from(Array(2001)).map((_, i) => `${i}px`) };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    {
+      pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+      variants: ['md'],
+    },
+    {
+      pattern: /h-(\d+)/,
+      variants: ['sm', 'md', 'lg'],
+    },
   ],
   theme: {
     extend: {
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_2000,
+      minHeight: px0_2000,
+      maxWidth: px0_2000,
+      maxHeight: px0_2000,
+      spacing: px0_200,
+      borderRadius: px0_100,
+      width: px0_2000,
+      height: px0_2000,
+      inset: px0_2000,
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'transparent': 'transparent',
+        'current': 'currentColor',
+        'sendy-blue-strong': '#192fa4',
+        'sendyBlue': '#2e58ec',
+        'sendy-blue-light': '#e3eeff',
+        'correct': '#1db964',
+        'white': '#ffffff',
+        'lightGrey': '#dedede',
+        'background-gray-light': '#f2f3f6',
+        'background-gray-lightest': '#fafbfc',
+        'gray-scale-blue-100': '#161820',
+        'gray-scale-blue-200': '#485464',
+        'gray-scale-blue-300': '#8a929e',
+        'gray-scale-blue-400': '#c3c9ce',
+        'gray-scale-blue-500': '#e4e8ed',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'slide-up': 'slideUp 0.3s ease-out forwards',
+        'slide-down': 'slideDown 0.3s ease-out forwards',
+        'gradientWave': 'gradientWave 3s ease-in-out infinite',
+      },
+      keyframes: {
+        slideUp: {
+          '0%': { transform: 'translateY(5%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        slideDown: {
+          '0%': { transform: 'translateY(0)', opacity: '1' },
+          '100%': { transform: 'translateY(5%)', opacity: '0' },
+        },
+        gradientWave: {
+          '0%, 100%': { backgroundPosition: '50% 0%' },
+          '50%': { backgroundPosition: '50% 100%' },
+        },
       },
     },
+    fontFamily: {
+      sans: ['Noto Sans KR', 'sans-serif'],
+      GMarket: ['GmarketSans', 'sans-serif'],
+      pretendard: [
+        'Pretendard Variable',
+        'Pretendard',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'system-ui',
+        'Roboto',
+        '"Helvetica Neue"',
+        '"Segoe UI"',
+        '"Apple SD Gothic Neo"',
+        '"Noto Sans KR"',
+        '"Malgun Gothic"',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        'sans-serif',
+      ],
+    },
+    screens: {
+      'laptop': { max: '1199px' },
+      'laptop-tablet': { max: '1199px', min: '992px' },
+      'lg': { max: '1023px' },
+      'tablet': { max: '991px' },
+      'tablet-md': { max: '991px', min: '768px' },
+      'md': { max: '767px' },
+      'mobile': { max: '394px' },
+    },
   },
-  plugins: [],
-} satisfies Config;
+};
