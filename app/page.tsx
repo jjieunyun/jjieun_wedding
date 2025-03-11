57593276672b2dc14b3cb7988b8a381d1c775e5f
jieun <lesson0186@gmail.com>
2025-03-12 01:59:22 +0900

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
         <main className="w-full h-full min-h-screen  flex items-center justify-center  bg-black ">
             <section className={'w-full h-full min-h-screen max-w-[740px] flex items-center bg-black'}>
                 <Image src={temp} alt={'temp'} className={'w-full h-screen object-contain'}/>
+                
             </section>
         </main>
     );
